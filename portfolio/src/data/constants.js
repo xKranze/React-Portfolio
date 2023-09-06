303503c94eaf6389daad353467007b2786dc0a2b
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-09-05 19:33:25 -0700

    added components structure

---
+++ portfolio/src/data/constants.js
@@ -0,0 +1,17 @@
+export const Bio = {
+    name: "Jimmy Yin",
+    roles: [
+      "Full Stack Developer",
+      "Freelance Web Developer",
+      "Tesla VSC Technician III",
+    ],
+    description:
+      "Web development and anything related to programming has always peaked my interest.  Wanting to challenge myself further, I took on the Full Stack Web Development course at Universtiy of Washington.  During this program I wad able to challenge myself and grow further engaging in a multitude of projects to further learn and sharpen my skills in both designing the look of web applications and building the behind the scenes functionality.",
+    github: "https://github.com/xKranze",
+    resume:
+      "#",
+    linkedin: "https://www.linkedin.com/in/jimmy-yin-5b2970266/",
+    twitter: "https://twitter.com/x_Kranze",
+    instagram: "https://www.instagram.com/xjyin/",
+    facebook: "https://www.facebook.com/jimmy.yin.1",
+  };
